b53acd5d2061ebd54bc5e419bee9c8d15d5c6dc6
Endy Muhardin <endy.muhardin@gmail.com>
2015-04-14 17:07:28 +0700

    enable OAuth di resource server

--- resource-server/src/main/java/com/muhardin/endy/belajar/stack2015/config/SecurityConfig.java
+++ resource-server/src/main/java/com/muhardin/endy/belajar/stack2015/config/SecurityConfig.java
@@ -6,9 +6,9 @@ import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.cloud.security.oauth2.sso.OAuth2SsoConfigurerAdapter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
@@ -17,15 +17,12 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.WebUtils;
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends OAuth2SsoConfigurerAdapter {
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-                .httpBasic()
-                .and().authorizeRequests()
-                    .antMatchers("/halo").permitAll()
-                    .anyRequest().authenticated()
+    public void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .anyRequest().authenticated()
                 .and().csrf().csrfTokenRepository(csrfTokenRepository())
                 .and().addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class);
     }
