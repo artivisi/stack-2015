f8d4cf9d9d405a2b67a2bc63ccb720a38058f6a9
Endy Muhardin <endy.muhardin@gmail.com>
2015-04-14 14:09:17 +0700

    starter project authorization server

---
+++ authorization-server/src/main/java/com/muhardin/endy/belajar/stack2015/AuthorizationServer.java
@@ -0,0 +1,13 @@
+package com.muhardin.endy.belajar.stack2015;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class AuthorizationServer {
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationServer.class, args);
+    }
+}
