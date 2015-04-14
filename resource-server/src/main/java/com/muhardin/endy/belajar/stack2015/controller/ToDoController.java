4d7665925d3193fb54deb6a275d82aa4155b89a9
Endy Muhardin <endy.muhardin@gmail.com>
2015-04-14 08:35:09 +0700

    spring boot starter project

---
+++ resource-server/src/main/java/com/muhardin/endy/belajar/stack2015/controller/ToDoController.java
@@ -0,0 +1,19 @@
+package com.muhardin.endy.belajar.stack2015.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ToDoController {
+    
+    @RequestMapping(value="/halo", method = RequestMethod.GET)
+    public Map<String, Object> halo(@RequestParam(required=false) String nama){
+        Map<String, Object> hasil = new HashMap<>();
+        hasil.put("salam", "Halo "+nama);
+        return hasil;
+    }
+}
