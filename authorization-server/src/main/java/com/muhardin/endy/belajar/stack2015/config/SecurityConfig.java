072806ca4add3dd5c8643bb117d1abbb7ee9a164
Endy Muhardin <endy.muhardin@gmail.com>
2015-05-12 10:18:39 +0700

    prevent concurrent login

--- authorization-server/src/main/java/com/muhardin/endy/belajar/stack2015/config/SecurityConfig.java
+++ authorization-server/src/main/java/com/muhardin/endy/belajar/stack2015/config/SecurityConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -47,9 +49,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         @Autowired
         private AuthenticationManager authenticationManager;
+        
+        @Bean 
+        public SessionRegistry sessionRegistry(){
+            return new SessionRegistryImpl();
+        }
 
         @Override
         protected void configure(HttpSecurity http) throws Exception {
+            http.sessionManagement()
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(true)
+                .sessionRegistry(sessionRegistry());
+            
             http
                     .formLogin().loginPage("/login").permitAll()
                     .and().requestMatchers().antMatchers("/login", "/oauth/authorize", "/oauth/confirm_access")
--- authorization-server/src/main/java/com/muhardin/endy/belajar/stack2015/config/WebConfig.java
+++ authorization-server/src/main/java/com/muhardin/endy/belajar/stack2015/config/WebConfig.java
@@ -1,10 +1,12 @@
 package com.muhardin.endy.belajar.stack2015.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 @Configuration @EnableResourceServer
 @PropertySource("classpath:clients.properties")
@@ -13,5 +15,10 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
+    }
+    
+    @Bean
+    public HttpSessionEventPublisher registerHttpSessionEventPublisher(){
+        return new HttpSessionEventPublisher();
     }
 }
