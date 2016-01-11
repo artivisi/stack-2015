3130721089e4d562ba7be7c48fc5038dd6e4645c
Adi Sulistiono <sulistiono.adi@gmail.com>
2016-01-11 14:10:33 +0700

    tambah konfig csrf repository

--- authorization-server/src/main/java/com/muhardin/endy/belajar/stack2015/config/SecurityConfig.java
+++ authorization-server/src/main/java/com/muhardin/endy/belajar/stack2015/config/SecurityConfig.java
@@ -1,6 +1,12 @@
 package com.muhardin.endy.belajar.stack2015.config;
 
 import com.muhardin.endy.belajar.stack2015.handler.Oauth2LogoutHandler;
+import java.io.IOException;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -19,7 +25,13 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CsrfFilter;
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+import org.springframework.security.web.csrf.HttpSessionCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.WebUtils;
 
 @EnableWebSecurity
 public class SecurityConfig {
@@ -90,29 +102,64 @@ public class SecurityConfig {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             http
-                    .sessionManagement()
-                    .maximumSessions(1)
-                    .maxSessionsPreventsLogin(true)
-                    .sessionRegistry(sessionRegistry());
-            
+                .sessionManagement()
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(false)
+                .sessionRegistry(sessionRegistry());
+
             http
-                    .authorizeRequests()
-                        .antMatchers("/login").permitAll()
-                        .antMatchers("/logout").permitAll()
-                        .antMatchers("/css/**").permitAll()
-                        .anyRequest().authenticated()
+                .authorizeRequests()
+                    .antMatchers("/login").permitAll()
+                    .antMatchers("/logout").permitAll()
+                    .antMatchers("/css/**").permitAll()
+                    .anyRequest().authenticated()
                 .and()
                     .formLogin().loginPage("/login").permitAll()
                 .and()
                     .logout()
                     .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
                     .invalidateHttpSession(true)
-                    .logoutSuccessHandler(logoutHandler);
+                    .logoutSuccessHandler(logoutHandler)
+                .and()
+                    .csrf().csrfTokenRepository(csrfTokenRepository())
+                    .and().addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class);
         }
         
         @Override
         protected void configure(AuthenticationManagerBuilder auth) throws Exception {
             auth.parentAuthenticationManager(authenticationManager);
+        }
+        
+        private CsrfTokenRepository csrfTokenRepository() {
+            HttpSessionCsrfTokenRepository repository = new HttpSessionCsrfTokenRepository();
+            repository.setHeaderName("X-XSRF-TOKEN");
+            return repository;
+        }
+    }
+    
+
+    private static class CsrfHeaderFilter extends OncePerRequestFilter {
+
+        @Override
+        protected void doFilterInternal(HttpServletRequest request,
+                HttpServletResponse response, FilterChain filterChain)
+                throws ServletException, IOException {
+            
+            logger.info("CSRF-TOKEN NAME ["+CsrfToken.class.getName()+"]");
+            CsrfToken csrf = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+            if (csrf != null) {
+                Cookie cookie = WebUtils.getCookie(request, "XSRF-TOKEN");
+                String token = csrf.getToken();
+                if(cookie!=null) logger.info("Cookie [" + cookie.getValue() + "]");
+                logger.info("Token [" + token + "]");
+                
+                if (cookie == null || token != null && !token.equals(cookie.getValue())) {
+                    cookie = new Cookie("XSRF-TOKEN", token);
+                    cookie.setPath("/auth-server/");
+                    response.addCookie(cookie);
+                }
+            }
+            filterChain.doFilter(request, response);
         }
     }
 }
