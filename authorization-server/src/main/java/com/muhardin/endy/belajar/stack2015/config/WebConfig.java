63992632ad281a011cabceb497b334185a48f72c
Adi Sulistiono <sulistiono.adi@gmail.com>
2016-06-17 01:11:45 +0700

    App Name and URL get from application.properteis

--- authorization-server/src/main/java/com/muhardin/endy/belajar/stack2015/config/WebConfig.java
+++ authorization-server/src/main/java/com/muhardin/endy/belajar/stack2015/config/WebConfig.java
@@ -2,13 +2,11 @@ package com.muhardin.endy.belajar.stack2015.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.security.web.session.HttpSessionEventPublisher;
 
 @Configuration
-@PropertySource("classpath:clients.properties")
 public class WebConfig extends WebMvcConfigurerAdapter {
     
     @Override
